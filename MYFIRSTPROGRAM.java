37185872597ce8dc77ee4689e751be268ead551c
GeorgeCamac <gcamac2015@gmail.com>
2021-09-16 18:28:53 -0400

    first commit

---
+++ MYFIRSTPROGRAM.java
@@ -0,0 +1,24 @@
+// MY FIRST PROGRAM
+class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+        int i = 2;
+        double j = 2.3;
+        float x = 1.22;
+        char ;
+        boolean ;
+        long  ;
+        string s = "asdasd"
+        const name = "george";
+
+
+    }
+}
+
+// boolean 1 byte
+// char 1 byte
+// double 8 byte
+// float 4 byte
+// int 4 byte
+// long 8 bytes
+// string infinite 
