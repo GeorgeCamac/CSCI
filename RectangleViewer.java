ebaf8cf3028bbdd35bfdaebcd232b62e0cea494f
GeorgeCamac <gcamac2015@gmail.com>
2021-09-21 19:23:30 -0400

    Added Friend Class

---
+++ RectangleViewer.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+
+public class RectangleViewer {
+  //  public static void main(String[] args) {
+    //    JFrame frame = new JFrame();
+    //frame.setsize(300,400);
+    //frame.setTitle("TwoRectangles");
+    //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //RectangleComponent component = newRectangleComponent
+    }
+    
+}
