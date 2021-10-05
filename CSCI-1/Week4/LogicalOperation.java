5d4da106e45f7e9d39cb01f15c607538d0c142e5
GeorgeCamac <gcamac2015@gmail.com>
2021-10-05 19:26:51 -0400

    fixing

--- CSCI-1/Week4/LogicalOperation.java
+++ CSCI-1/Week4/LogicalOperation.java
@@ -1,8 +1,23 @@
 package Week4;
 
 import java.util.Scanner;
-    
-    public static void main(String[] args) {
+    public class LogicalOperation{
+        public static void main(String[] args) {
+            int temp = 20;
+
+        boolean rain = false; {
+
+        if(temp <70 && rain != true){
+            System.out.println("You do not need a jacket");
+        }
+        else if(temp > 30 && temp <=70 && rain != true){
+            System.out.println("You will need a light jacket");
+        }
+        else{
+            System.out.println("You need a heavy jacket or it is raining");
+        }
+        }
+        }
         // int temp = 40;
 
         // if (temp > 70){
@@ -19,21 +34,6 @@ import java.util.Scanner;
         // else{
         //     System.out.println("You need a heavy jacket");
         // }
-
-        // int temp = 20;
-
-        // boolean rain = false; 
-
-        // if(temp <70 && rain != true){
-        //     System.out.println("You do not need a jacket");
-        // }
-        // else if(temp > 30 && temp <=70 && rain != true){
-        //     System.out.println("You will need a light jacket");
-        // }
-        // else{
-        //     System.out.println("You need a heavy jacket or it is raining");
-        // }
-    
     // Scanner scanner = new Scanner(System.in);
     
     //     System.out.println("Please select menu option. Press q or Q to quit");
